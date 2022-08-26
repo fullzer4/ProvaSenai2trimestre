380eba7b5e684bca7842609cc51e66143d176741
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-26 11:43:11 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 array = []
 
-function operacao(numero1, numero2){
-
-}
-
 function limpar(){
-
-}
+    Altura = document.getElementById("altura").value = ""
+    Peso = document.getElementById("peso").value = ""
+    Resultado = document.getElementById("result").value = ""
+}   
 
 function calcular(){
-
-}
-
-function salvar(){
-    
+    Altura = document.getElementById("altura").value
+    Peso = document.getElementById("peso").value
+    Finalaltura = Math.pow(Altura, 2)
+    Resultado = document.getElementById("result").value = Peso / Finalaltura
+    array.push(Resultado)
+    console.log(array)
 }
